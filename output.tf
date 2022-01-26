520338a8453acefa5c28b6fa70cf6dd88f45b6c2
Juan Herrera <juan.herrera@suse.com>
2022-01-26 14:53:19 +0100

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "cluster_id" {
-  value       = data.rancher2_cluster.local.id
-  description = "Rancher local cluster id"
-}
 
 output "access_key_foo" {
     value = rancher2_token.auth.access_key
